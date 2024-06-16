7443ea41de1ccdc028b7bd806df5e62dbc390db6
user <soochan7@gmail.com>
2024-06-16 18:17:36 +1000

    trying to making create Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Create from './CreatingIssue';
 import './App.css';
 
 function App() {
+	const [issue, setIssue] = useState([]);
+  	
+  	useEffect(() => {
+		const fetchData = async() => {
+          const res = await fetch('http://localhost:8080');
+          const result = res.json();
+          console.log(result)
+          return result;
+        }	
+        fetchData().then(res => setIssue(res));
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="black-nav">
+        <div>issue_client</div>
+      </div>      
+      {
+        issue.map(function(a, i) {
+          return <div className="list" key={i}>
+            
+            {issue[i].id}
+            <h4>{issue[i].title}</h4>
+            {issue[i].description}
+          
+        </div>
+        })
+      }    
+    </div> 
+  )
 }
+
 
 export default App;
---
+++ src/CreatingIssue.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function CreatingIssues() {
+    return (
+
+        <div>
+        <form>
+        
+        </form>
+        </div>
+
+    )
+    
+    
+}
+
+export default CreatingIssues;
